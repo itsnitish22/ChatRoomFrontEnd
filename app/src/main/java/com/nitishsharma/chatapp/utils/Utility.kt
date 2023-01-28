eb5db780d0ad4ae41a1a95f52303e719bcb62d9c
Nitish Sharma <nitish.sharma1186@gmail.com>
2023-01-28 22:22:37 +0530

    new app icon

--- app/src/main/java/com/nitishsharma/chatapp/utils/Utility.kt
+++ app/src/main/java/com/nitishsharma/chatapp/utils/Utility.kt
@@ -4,6 +4,6 @@ import com.nitishsharma.chatapp.BuildConfig
 
 object Utility {
     const val SERVER_PATH = "wss://chatappbackendws.azurewebsites.net/"
-    const val SERVER_PATH_PIESOCET_TEST =
+    const val SERVER_PATH_PIESOCKET_TEST =
         "wss://${BuildConfig.PIESOCKET_CLUSTER_ID}.piesocket.com/v3/${BuildConfig.PIESOCKET_ROOM_ID}?api_key=${BuildConfig.PIESOCKET_API_KEY}&notify_self=1"
 }
