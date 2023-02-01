5ba4bf051a34f360d2f460709aadd97f7a2d916d
Nitish Sharma <nitish.sharma1186@gmail.com>
2023-02-01 18:22:42 +0530

    cleanup

--- app/src/main/java/com/nitishsharma/chatapp/onboarding/OnboardingFragment.kt
+++ app/src/main/java/com/nitishsharma/chatapp/onboarding/OnboardingFragment.kt
@@ -2,6 +2,7 @@ package com.nitishsharma.chatapp.onboarding
 
 import android.app.Activity
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -18,23 +19,23 @@ import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
 import com.nitishsharma.chatapp.R
-import com.nitishsharma.chatapp.databinding.FragmentRegisterBinding
+import com.nitishsharma.chatapp.databinding.FragmentOnboardingBinding
 
 
-class RegisterFragment : Fragment() {
+class OnboardingFragment : Fragment() {
     private lateinit var auth: FirebaseAuth
-    private lateinit var binding: FragmentRegisterBinding
+    private lateinit var binding: FragmentOnboardingBinding
     private lateinit var googleSignInClient: GoogleSignInClient
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentRegisterBinding.inflate(inflater, container, false)
-        auth = FirebaseAuth.getInstance()
-        auth.signOut()
-        setGooglePlusButtonText(binding.signInWithGoogle, "Authenticate to continue")
+        binding = FragmentOnboardingBinding.inflate(inflater, container, false)
 
+        initializeInstances() //initialize instances
+
+        //on click signInWithGoogle
         binding.signInWithGoogle.setOnClickListener {
             authenticateWithGoogle()
         }
@@ -42,43 +43,15 @@ class RegisterFragment : Fragment() {
         return binding.root
     }
 
-    private fun authenticateWithGoogle() {
-        val signInIntent = googleSignInClient.signInIntent
-
-        launcher.launch(signInIntent)
+    //initializing instances
+    private fun initializeInstances() {
+        auth = FirebaseAuth.getInstance()
+        initializeGoogleAuthentication()
+        setGooglePlusButtonText(binding.signInWithGoogle, "Authenticate to continue")
     }
 
-    private val launcher =
-        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-            if (result.resultCode == Activity.RESULT_OK) {
-
-                val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
-                handleResults(task)
-            }
-        }
-
-    private fun handleResults(task: Task<GoogleSignInAccount>) {
-        if (task.isSuccessful) {
-            val account: GoogleSignInAccount? = task.result
-            if (account != null) {
-                updateUI(account)
-            }
-        } else {
-            Toast.makeText(requireContext(), task.exception.toString(), Toast.LENGTH_SHORT).show()
-        }
-    }
-
-    private fun updateUI(account: GoogleSignInAccount) {
-        val credential = GoogleAuthProvider.getCredential(account.idToken, null)
-        auth.signInWithCredential(credential).addOnCompleteListener {
-            if (it.isSuccessful) {
-                Toast.makeText(requireContext(), "Success", Toast.LENGTH_SHORT).show()
-            } else {
-            }
-        }
-    }
-
-    private fun setGooglePlusButtonText(signInButton: SignInButton, buttonText: String?) {
+    //initializing google authentication
+    private fun initializeGoogleAuthentication() {
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
             .requestIdToken(getString(R.string.default_web_client_id))
             .requestEmail()
@@ -86,7 +59,47 @@ class RegisterFragment : Fragment() {
 
         googleSignInClient = GoogleSignIn.getClient(requireContext(), gso)
         googleSignInClient.revokeAccess()
+    }
 
+    //authenticating with google
+    private fun authenticateWithGoogle() {
+        val signInIntent = googleSignInClient.signInIntent
+        launcher.launch(signInIntent)
+    }
+
+    //launcher dealing w results
+    private val launcher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+                handleResults(task)
+            }
+        }
+
+    //handling google authenticate results
+    private fun handleResults(task: Task<GoogleSignInAccount>) {
+        if (task.isSuccessful) {
+            val account: GoogleSignInAccount? = task.result
+            if (account != null) {
+                updateUI(account)
+            }
+        } else {
+            Log.i("OnboardingFrag", "Failed login task")
+        }
+    }
+
+    //updating the ui on successful authentication
+    private fun updateUI(account: GoogleSignInAccount) {
+        val credential = GoogleAuthProvider.getCredential(account.idToken, null)
+        auth.signInWithCredential(credential).addOnCompleteListener {
+            if (it.isSuccessful) {
+                Toast.makeText(requireContext(), "Success", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    //setting up text on google authenticate button
+    private fun setGooglePlusButtonText(signInButton: SignInButton, buttonText: String?) {
         for (i in 0 until signInButton.childCount) {
             val v = signInButton.getChildAt(i)
             if (v is TextView) {
