94eb03f79bfa3a27cb54f9487083504570c146d9
Nitish Sharma <nitish.sharma1186@gmail.com>
2023-02-01 13:48:57 +0530

    firebase auth implemented

--- app/src/main/java/com/nitishsharma/chatapp/onboarding/RegisterFragment.kt
+++ app/src/main/java/com/nitishsharma/chatapp/onboarding/RegisterFragment.kt
@@ -1,17 +1,98 @@
 package com.nitishsharma.chatapp.onboarding
 
+import android.app.Activity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.SignInButton
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import com.nitishsharma.chatapp.R
+import com.nitishsharma.chatapp.databinding.FragmentRegisterBinding
+
 
 class RegisterFragment : Fragment() {
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: FragmentRegisterBinding
+    private lateinit var googleSignInClient: GoogleSignInClient
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_register, container, false)
+        binding = FragmentRegisterBinding.inflate(inflater, container, false)
+        auth = FirebaseAuth.getInstance()
+        auth.signOut()
+        setGooglePlusButtonText(binding.signInWithGoogle, "Authenticate to continue")
+
+        binding.signInWithGoogle.setOnClickListener {
+            authenticateWithGoogle()
+        }
+
+        return binding.root
+    }
+
+    private fun authenticateWithGoogle() {
+        val signInIntent = googleSignInClient.signInIntent
+
+        launcher.launch(signInIntent)
+    }
+
+    private val launcher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+
+                val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+                handleResults(task)
+            }
+        }
+
+    private fun handleResults(task: Task<GoogleSignInAccount>) {
+        if (task.isSuccessful) {
+            val account: GoogleSignInAccount? = task.result
+            if (account != null) {
+                updateUI(account)
+            }
+        } else {
+            Toast.makeText(requireContext(), task.exception.toString(), Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun updateUI(account: GoogleSignInAccount) {
+        val credential = GoogleAuthProvider.getCredential(account.idToken, null)
+        auth.signInWithCredential(credential).addOnCompleteListener {
+            if (it.isSuccessful) {
+                Toast.makeText(requireContext(), "Success", Toast.LENGTH_SHORT).show()
+            } else {
+            }
+        }
+    }
+
+    private fun setGooglePlusButtonText(signInButton: SignInButton, buttonText: String?) {
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(requireContext(), gso)
+        googleSignInClient.revokeAccess()
+
+        for (i in 0 until signInButton.childCount) {
+            val v = signInButton.getChildAt(i)
+            if (v is TextView) {
+                v.text = buttonText
+                return
+            }
+        }
     }
 }
