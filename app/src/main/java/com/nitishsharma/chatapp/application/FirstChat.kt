bf421d9b99d34a44ef7e14143abd616e5805242b
Nitish Sharma <nitish.sharma1186@gmail.com>
2023-02-28 22:08:31 +0530

    fcm integrated, background working, foreground to be done, broadcast message done

--- app/src/main/java/com/nitishsharma/chatapp/application/FirstChat.kt
+++ app/src/main/java/com/nitishsharma/chatapp/application/FirstChat.kt
@@ -1,6 +1,9 @@
 package com.nitishsharma.chatapp.application
 
 import android.app.Application
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.FirebaseApp
+import com.google.firebase.messaging.FirebaseMessaging
 import com.nitishsharma.chatapp.BuildConfig
 import io.socket.client.IO
 import io.socket.client.Socket
@@ -15,5 +18,17 @@ class FirstChat : Application() {
         super.onCreate()
         socketIO?.connect()
         plant(Timber.DebugTree())
+        FirebaseApp.initializeApp(this)
+        getFirebaseMessagingToken()
+    }
+
+    private fun getFirebaseMessagingToken() {
+        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                Timber.tag("FirstChat").w(task.exception, "Fetching FCM registration token failed")
+                return@OnCompleteListener
+            }
+            Timber.tag("FCM Token").d(task.result)
+        })
     }
 }
